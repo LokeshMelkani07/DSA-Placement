864b3c059c57b0e9b580d04598e3c1e5c47ae056
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2023-05-06 11:07:51 +0530

    pointer basic part 1

--- 11_day/0.cpp
+++ 11_day/0.cpp
@@ -1,65 +1,85 @@
 #include<iostream>
 #include<vector>
-#include<unordered_set>
+#include<algorithm>
 using namespace std;
 
-  vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> set1(nums1.begin(), nums1.end());
-        unordered_set<int> set2(nums2.begin(), nums2.end());
+// Pointers in C++
 
-        vector<int> numss1, numss2;
-        for (int num : set1) {
-            if (set2.count(num) == 0) {
-                numss1.push_back(num);
-            }
-        }
+int main()
+{
+    // symbol table will map num with some mememory address where 5 is stored
+    int num = 5;
+    cout<<num<<endl;
 
-        for (int num : set2) {
-            if (set1.count(num) == 0) {
-                numss2.push_back(num);
-            }
-        }
+    // address of operator (&)
+    cout<<"address of num is "<<&num<<endl;
 
-        return {numss1, numss2};
-    }
-    void update(int *p){
-        *p = (*p)  *  2;
-    }
-        void increment(int **p){
-        ++(**p);
-    }
-
-
-int main () {
-
-    int num  = 5 ;
-
-    cout << num << endl;
-
-    // address of Operator - &
-
-    cout <<" address of num is " << &num << endl;
-
+    // Pointer
+    // Always initialise a pointer either by 0 if nothing a vaiable but always initialise it
     int *ptr = &num;
-
-    cout << "Address is : " << ptr << endl;
-    cout << "value is : " << *ptr << endl;
-
-    double d = 4.3;
+    cout<<"Address at ptr is "<< ptr<<endl;
+    cout<<"Value at pointer is "<<*ptr<<endl; // 6
+    num++;
+    cout<<"Value at pointer after increment is "<<*ptr<<endl;  // 6
+    // pointer can be of different data type
+    double d = 5.4;
     double *p2 = &d;
 
-    cout << "Address is : " << p2 << endl;
-    cout << "value is : " << *p2 << endl;
+    cout<<p2<<endl;
+    cout<<*p2<<endl;
 
-    cout << " size of integer is " << sizeof(num) << endl;
-    cout << " size of pointer is " << sizeof(ptr) << endl;
-    cout << " size of pointer is " << sizeof(p2) << endl;
+    // Size of pointer
+    cout<<"Size of int data type is "<<sizeof(int)<<endl; // 4
+    cout<<"Size of pointer to int data type is "<<sizeof(ptr)<<endl; // 4
+    cout<<"Size of double data type is "<<sizeof(d)<<endl;  // 8
+    cout<<"Size of pointer to double data type is "<<sizeof(p2)<<endl;  // 4
+
+    // pointer to int is created which is pointing to some garbage value [BAD PRACTICE]
+    //  int *p;
+    int *p = 0;  // atleast do this as segamentation fault will come as 0 is not a memory whihc exist
+
+    // Other way of storing value in pointer
+    int i = 150;
+    int *p1 = 0;
+    p1 = &i;
+    cout<<p1<<endl;
+    cout<<*p1<<endl;
+
+    int num1 = 5;
+    int *p3 = &num1; // reference to that block means the address is stored in p3 so any change in value will be reflected in p3 also
+    int a = num1; // copy of num1 is stored in a
+    num1++; // if we ++ num1, it will not affect a
+    cout<<num1<<endl; // 6
+    cout<<a<<endl;  // 5
+    cout<<*p3<<endl;  // 6
+    (*p3)++;  // *p3 means value at num1 address, which is 6 currently, *p3++ means 6->7
+    cout<<num1<<endl; // 7
+    cout<<*p3<<endl;  // 7
 
 
-    int num1 = 110;
-    int *ptr = &num1;
-    increment(&ptr);
-    cout << num1 << endl;
+    // Copying pointers
+    int *q = p3;
+    cout<<"Address at p and q is "<<"p3 "<<p3<<" "<<" q "<<q<<endl; // same address of both
+    cout<<"value at p and q is "<<"p3 "<<*p3<<" "<<" q "<<*q<<endl; // 7 , 7
+    (*p3)++;
+    cout<<"value at p and q is "<<"p3 "<<*p3<<" "<<" q "<<*q<<endl;  // 8 , 8
 
+
+    // Pointer Arithmetic
+    int j = 9;
+    int *p4  = &j;
+    cout<<(*p4)++<<endl; // 9
+    // p4 is now 10
+    *p4 = *p4 + 1;  // p4 is now 1+1 = 11
+    cout<<*p4<<endl; // 11
+    cout<<p4<<endl;  // 0x61fe84
+    p4 = p4+1;  // it will add 4 bytes in the memory address and move to next integer
+    cout<<p4<<endl; // 0x61fe88
+    cout<<*p4<<endl; //  8
+
+    // Void pointer
+    // Pointer of type vpid which can be typecasted to any other type. void *ptr;
+    // Wild pointer
+    // which is not initialsed and is pointing to any random garbage value
     return 0;
 }
--- 11_day/1.cpp
+++ 11_day/1.cpp
@@ -1,47 +1,11 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
-int& func(int a) {
-    int num = a;
-    int& ans = num;
-    return ans;
-}
+// Pointers in C++
 
-int* fun(int n) {
-    int* ptr = &n;
-    return ptr;
-}
-
-void update2(int& n) {
-    n++;
-}
-
-void update1(int n) {
-    n++;
-}
-
-int main() {
-
-    /*
-    int i = 5;
-    //create a ref variable
-    int& j = i;
-    cout << i << endl;
-    i++;
-    cout << i << endl;
-    j++;
-    cout << i << endl;
-    cout << j << endl;
-    */
-
-   int n = 5;
-
-   cout << "Before " << n << endl;
-   update2(n);
-   cout << "After " << n << endl;
-
-
-    fun(n);
-
+int main()
+{
     return 0;
 }
