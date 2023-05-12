a2c1e41bb0d1595b13ce00a02b1d367f4a66ef85
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2023-05-12 11:48:16 +0530

    OOPS Part 1 Completed

--- 14_day/0.cpp
+++ 14_day/0.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cstring>
 // Accessing hero.cpp from outside which has a class of type Hero, we can use it here also but we need to include Hero.cpp like this
 // #include "Hero.cpp"
 #include<vector>
@@ -22,7 +23,7 @@ class Hero{
     */
 
     // Properties
-    char name[10];
+    // char name[10];
 
     /*
 
@@ -33,8 +34,15 @@ class Hero{
     */
 
     public:
+    char *name;
     int health;
     char level;
+
+    // static keyword makes an data member which belongs to class
+    // That is to access that data member we do not need to make any object
+    // We need to initialise static member outside class suing :: (scope resoltuion operator)
+    // int Hero::timetoComplete = 10;
+    static int timeToComplete;
 
     // Constructor
     // Once we make this constructor ourself, the default cons which is made by the CPU will get automatically destroyed
@@ -42,6 +50,7 @@ class Hero{
     Hero()
     {
         cout<<"Constructor called"<<endl;
+        name = new char[100];
     }
 
 
@@ -65,18 +74,30 @@ class Hero{
 
     // Our own implementation of Copy Constructor
     // We do pass by reference in Copy constructor so that real Suresh object is passed directly and copies are not formed
+
     Hero(Hero &temp)
     {
+        // Default constructor does shallow copy (Same memory location, called by 2 different names)
+        // To do deep copy (Different memory copy is made) we use our own Copy constructor
+
+        char *ch = new char[strlen(temp.name) +1];
+
+        strcpy(ch,temp.name);
+
         cout<<"Copy Constructor is called"<<endl;
         // Once we write our own implementation of Copy constructor, the default copy constrcutor gets destroyed
         this->health = temp.health;
         this->level = temp.level;
+        // Deep copy of name is shared
+        this->name = ch;
     }
+
 
     void print()
     {
         cout<<"Level is "<<this->level<<endl;
         cout<<"Health is "<<this->health<<endl;
+        cout<<"Name is "<<this->name<<endl;
     }
 
     // When our variable is private and we need to set it and get it
@@ -113,8 +134,41 @@ class Hero{
         // Write
         level = ch;
     }
+
+    void setName(char *name)
+    {
+        strcpy(this->name,name);
+    }
+
+
+    // Static Functions
+    // Same as Staic variable
+    // These do not have this keyword as this means pointer to current object and here we do not have any object so it does not have this keyword
+    // Static function can onllly access
+    static int random()
+    {
+        // cout<<this->health<<endl;  // error
+        return timeToComplete;
+    }
+
+    // Destructor
+    // This is a memory de-allocater
+    // When object is getting out of scope or is dieing then destructor is called to free up that memory
+    // Name of destructor = name of class
+    // No return type
+    // No parameter
+    // Destructor is automatically made but we can make it manually also
+    // we need to use ~ sign
+    // More on destructor inside int main() line 320
+    ~Hero()
+    {
+        cout<<"Destructor is called"<<endl;
+    }
+
 };
 
+// Static Variable initialisation
+int Hero::timeToComplete = 10;
 
 
 int main()
@@ -232,16 +286,112 @@ int main()
     // So this is copy constructor which copies the value from one object to other
 
 
+    // Shallow and Deep Copy
+    // Default copy constructor does the shallow copy
+    Hero h3;
+    h3.setHealth(100);
+    h3.setLevel('M');
+    char name[7] = "lokesh";
+    h3.setName(name);
+
+    h3.print();
+
+    // Use default copy const and make another Hero
+    Hero h4(h3);
+    Hero h5 = h3; // another way of doing above work
+    cout<<"h4 printing"<<endl;
+    h4.print(); // name = mokesh
+    cout<<"h3 printing after changing lokesh to mokesh"<<endl;
+    h3.name[0] = 'M';
+    h3.print(); // name = mokesh
+    cout<<"h4 printing after changing h3 lokesh to mokesh"<<endl;
+    h4.print();  // It also changes to mokesh
+    // So default constructor does the shallow copy which means
+    // We access same memory from 2 different name h3 and h4
+    // Sameway Deep copy means, Another copy of same thing will be made and changes do not get reflected
+    // To implement Shallow copy, we have default constructor
+    // To implemement Deep copy we need to implement it through Our own copy constructor
+    h4.print(); // gives lokesh now because now we have made a deep copy of it
+
+    // Copy Assignment Operator
+    // Let say h3 and h4 are two objects with some value of health and level
+    // We write h3 = h4 which means h3.health = h4.health and h3.level = h4.level, h3.name = h4.name
+    // h4 ka maal h3 mai aa jayega
+    cout<<"Assigning h3 material inside h5 which is empty currently"<<endl;
+    h5 = h3;
+    cout<<"Printing h3 in h5"<<endl;
+    h5.print(); // name = mokesh
 
 
+    // Destructor
+    // This is a memory de-allocater
+    // When object is getting out of scope or is dieing then destructor is called to free up that memory
+    // Name of destructor = name of class
+    // No return type
+    // No parameter
+    // Destructor is automatically made but we can make it manually also
+    // we need to use ~ sign
+    // Once we write our own implementationof an destructor, the default destuctor gets killed automatically
+    // For any object constructor is called only once (during its birth)
+    // For any object destructor is called only once (during its death)
+    /*
+    ~Hero()
+    {
+        cout<<"Destructor is called"<<endl;
+    }
+    */
+    // For object made by static allocation, destructor is called automatically
+    // For object made by dynamic allocation, we need to call the desctructor manually
+    // Dynamically allocated object h6
+    Hero *h6 = new Hero();
+    // manually calling the destructor
+    delete h6;
 
 
+    // When do we use Initializer List in C++?
+    // Initializer List is used in initializing the data members of a class. The list of members to be initialized is indicated with constructor as a comma-separated list followed by a colon. Following is an example that uses the initializer list to initialize x and y of Point class.
+    /*
+    class Point {
+        private:
+        int x;
+        int y;
+        public:
+    Point(int i = 0, int j = 0):x(i), y(j) {}
+    /*  The above use of Initializer list is optional as the
+    constructor can also be written as:
+    Point(int i = 0, int j = 0) {
+                x = i;
+                y = j;
+    }
+    */
+
+    // int getX() const {return x;}
+    // int getY() const {return y;}
+// };
 
 
+    // Static Keyword
+    // static keyword makes an data member which belongs to class
+    // That is to access that data member we do not need to make any object
+    // We need to initialise static member outside class suing :: (scope resoltuion operator)
+    // int Hero::timetoComplete = 10;
+    // static int timeToComplete;
+    // Accessing Static member which is initialised outside class and declared inside class
+    cout<<"Hero ka time to complete is "<<Hero::timeToComplete<<endl;
+    // we can also access it using a object but that is not needed
 
-
-
-
+    // Static Functions
+    // Same as Staic variable
+    // These do not have this keyword as this means pointer to current object and here we do not have any object so it does not have this keyword
+    // Static function can onllly access static members
+    /*
+    static int random()
+    {
+        // cout<<this->health<<endl;  // error
+        return timeToComplete;
+    }
+    */
+    cout<<Hero::random()<<endl;
 
 
 
