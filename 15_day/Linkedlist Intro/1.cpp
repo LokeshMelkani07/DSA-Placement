d0d14c6381ab4b2041332b87cbcf17a7cb4fce8a
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2023-05-13 22:36:33 +0530

    Linkedlist intro

---
+++ 15_day/Linkedlist Intro/1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+// Doubly Linkedlist
+// Lecture 44: Linked List & its types - Singly, Doubly, Circular etc.
+
+
+int main()
+{
+    return 0;
+}
---
+++ 15_day/Linkedlist Intro/2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+// Circular Linkedlist
+// Lecture 44: Linked List & its types - Singly, Doubly, Circular etc.
+
+int main()
+{
+    return 0;
+}
