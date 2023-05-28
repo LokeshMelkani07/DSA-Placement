7a004117baf7d44c7ccd01af5719b411a0c1d965
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2023-05-28 14:13:28 +0530

    Minimum Spanning tree || prim's algorithm

---
+++ 15_day/11.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<unordered_map>
+#include<list>
+#include<algorithm>
+#include<vector>
+#include<limits>
+using namespace std;
+
+// Lecture 96: Minimum Spanning Tree || Prim's Algorithm
+// TC: O(n^2) but can be optimised to O(nlogn) if we use priority queue to get the mini element
+
+vector<pair<pair<int, int>, int>> calculatePrimsMST(int n, int m, vector<pair<pair<int, int>, int>> &g)
+{
+    // Our Node starts from 1 to n
+    // Creating an adjacency list
+    unordered_map<int,list<pair<int,int>>> adj;
+    for(int i = 0 ;i<g.size();i++)
+    {
+        int u1 = g[i].first.first;
+        int v1 = g[i].first.second;
+        int w1 = g[i].second;
+
+        // As it is undirected graph so
+        adj[u1].push_back({v1,w1});
+        adj[v1].push_back({u1,w1});
+    }
+
+    // We need 3 Data structures
+    // Size is n+1 as our node starts from 1
+    vector<int> key(n+1);
+    vector<int> parent(n+1);
+    vector<bool> mst(n+1);
+
+    // Initialise them
+    for(int i = 1;i<=n;i++)
+    {
+        key[i] = INT_MAX;
+        parent[i] = -1;
+        mst[i] = false;
+    }
+
+    // Let source node be 1 so mark it
+    key[1] = 0;
+    parent[1] = -1;
+
+    // Processing starts for each node
+    for(int i = 1;i<n;i++)
+    {
+        int mini = INT_MAX;
+        int u;
+
+        // Find minimum value from key array
+        // The below for loop takes o(n) to find the minimum but we can use priority queue min heap to get minimum element in o(1)
+        for(int v = 1;v<=n;v++)
+        {
+            // Keep the condition in mind that mst[index] should be false
+            if(mst[v]==false && key[v]<mini)
+            {
+                u = v;
+                mini = key[v];
+            }
+        }
+
+        // Mark it mst
+        mst[u] = true;
+
+        // Explore its neighbours
+        for(auto it: adj[u])
+        {
+            int v = it.first; // node adjacent to u in adj list
+            int w = it.second; // weight value of node adjacent to u in adj list
+
+            // if current value is smaller than stored value then modify it
+            if(mst[v]==false && key[v]>w)
+            {
+                parent[v] = u;
+                key[v] = w;
+            }
+        }
+
+    }
+
+    // Now we just need our parent array and store things in result array
+    vector<pair<pair<int, int>, int>> result;
+
+    // We start the index from 2 as parent of 1 is -1 which means 1 is the starting node
+    for(int i = 2;i<=n;i++)
+    {
+      // {{parent,child}, weight} is format of our result array
+      result.push_back({{parent[i],i},key[i]});
+    }
+
+    // return result
+
+    return result;
+
+}
+
+
+int main()
+{
+    return 0;
+}
---
+++ 15_day/16.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<unordered_map>
+#include<list>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
