7e8fbaa464113cc745f21243777c8f686bf06358
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2023-05-28 11:58:42 +0530

    Shortest path in DAG completed

--- 15_day/9.cpp
+++ 15_day/9.cpp
@@ -7,78 +7,74 @@
 using namespace std;
 
 // Lecture 94: Shortest Path in Directed Acyclic Graphs
+// TC: O(N+E)
+// SC: O(N+E) for adj list, stack space, distance array, visited array
 
 class graph{
 
 	public:
 
+	// Here as it is a Directed Acyclic graph with some weights so we use list<pair<int,int>> to store node value and weight
 	unordered_map<int ,list<pair<int, int>>> adj;
 
-
+	// Adding the edge in the graph
 	void addEdge(int a, int b, int weight){
-
 		pair<int ,int> p = make_pair(b, weight);
-
 		adj[a].push_back(p);
 	}
 
-	void printAdj(){
-
-	for(auto i:adj){
-	cout<<i.first<<"->";
-		for(auto j: i.second){
-			cout<<"[ "<<j.first<<","<<j.second<<" ]";
+	void printAdj()
+	{
+		for(auto i:adj){
+		cout<<i.first<<"->";
+			for(auto j: i.second){
+				cout<<"[ "<<j.first<<","<<j.second<<" ]";
+			}
+			cout<<endl;
 		}
-		cout<<endl;
-	}
-
 	}
 
 
-	void getShortestPath(int src ,vector<int> &dist, stack<int> &s ){
+	// For topological sort
+	void dfs(int node ,unordered_map<int , bool> &visited,  stack<int> &s)
+	{
 
+		visited[node] = 1;
+		// neighbour is an pair in this case
+		for(auto neighbour :adj[node]){
+			if(!visited[neighbour.first]){
+				dfs(neighbour.first, visited, s);
+			}
+		}
+		s.push(node);
+    }
+
+	// Processing to prepare the distance array
+	void getShortestPath(int src ,vector<int> &dist, stack<int> &s )
+	{
+		// Mark distance of source node = 0
 		dist[src]=0;
-
-
-
 		while(!s.empty()){
 
+			// Get the top element
 			int ele = s.top();
 			s.pop();
 
 			if(dist[ele] != INT_MAX){
 
+				// Update the distance for the neighbouring node
 				for(auto i :adj[ele]){
-					if(dist[i.first] > dist[ele] + i.second){
+					// If value present is more then value found then update it in distance array
+					if(dist[i.first] > dist[ele] + i.second)
+					{
 						dist[i.first] = dist[ele] + i.second;
 					}
 				}
 			}
 
 		}
-
-
-
-
 	}
-
-
-
-	void dfs(int node ,unordered_map<int , bool> &visited,  stack<int> &s){
-
-	visited[node] = 1;
-
-
-	for(auto i :adj[node]){
-		if(!visited[i.first]){
-			dfs(i.first, visited, s);
-		}
-	}
-	s.push(node);
-}
 };
-
-
 
 int main(){
 
@@ -96,6 +92,7 @@ int main(){
 
 	g.printAdj();
 
+	// Finding the topological sort order
 	int n=6;
 
 	unordered_map<int , bool> visited;
@@ -107,32 +104,22 @@ int main(){
 		}
 	}
 
-
-
-
+	// Creating ditance array initialise with infinity which will store the shortest path
 	vector<int> dist(n);
-
 	for(int i=0;i<n;i++){
 
 		dist[i] = INT_MAX;
 	}
 
+	// Let our source node be 1
 	int src =1;
 
-
-
+	// Calling shortest path function
 	g.getShortestPath(src, dist, s);
-
-
 	cout<<"Answer is " <<endl;
 	for(int i=0;i<dist.size();i++){
 		cout<<dist[i]<<" ";
 	}
 	return 0;
 
-}
-
-int main()
-{
-    return 0;
 }
