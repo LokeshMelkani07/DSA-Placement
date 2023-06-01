62b4018e016493ffcc0f92e346b2735cddd946f5
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2023-06-01 12:05:45 +0530

    Kruskal's Algo, disjoint set and union, path compression

--- 15_day/12.cpp
+++ 15_day/12.cpp
@@ -7,81 +7,100 @@ using namespace std;
 
 // Lecture 97: Kruskal's Algorithm || Disjoint Set || Union by Rank & Path Compression
 
-class DisjointSet{
-    vector<int> size,parent, rank;
-public:
-    DisjointSet(int n){
-        rank.resize(n+1,0);
-        size.resize(n+1);
-        parent.resize(n+1);
-        for(int i=0;i<=n;i++){
-            parent[i] = i;
-            size[i] = 1;
-        }
-    }
-
-    int findParent(int node){
-        if(node == parent[node]) return node;
-        return parent[node] = findParent(parent[node]);
-    }
-
-    void RankByUnion(int u,int v){
-        int ulti_p_u = findParent(u);
-        int ulti_p_v= findParent(v);
-        if(ulti_p_u == ulti_p_v )return;
-        if(rank[ulti_p_u]<rank[ulti_p_v])
-            parent[ulti_p_u] = ulti_p_v;
-        else if(rank[ulti_p_u]> rank[ulti_p_v])
-            parent[ulti_p_v] = ulti_p_u;
-        else{
-            parent[ulti_p_v] = ulti_p_u;
-            rank[ulti_p_u]++;
-        }
-    }
-
-    void SizeByUnion(int u,int v){
-        int ulti_p_u = findParent(u);
-        int ulti_p_v = findParent(v);
-
-        if(ulti_p_u == ulti_p_v) return;
-        if(size[ulti_p_u] < size[ulti_p_v]){
-            parent[ulti_p_u] = ulti_p_v;
-            size[ulti_p_v] += size[ulti_p_u];
-        }
-
-        else {
-            parent[ulti_p_v] = ulti_p_u;
-            size[ulti_p_u] += size[ulti_p_v];
-        }
-
-    }
-};
-
-int minimumSpanningTree(vector<vector<int>>&adj, int V)
+// TC: O(mlogm) where m is number fo edges
+// SC: O(n)
+bool cmp(vector<int> &a,vector<int> &b)
 {
-        vector<pair<int,pair<int,int>>>edges;
-        for(auto it:adj){
-          int u = it[0];int v=  it[1];
-          int w =it[2];
-        edges.push_back({w,{u,v}});
-        }
-        int mxCnt =0;
-    sort(edges.begin(),edges.end());
-        DisjointSet st(V);
-        for(auto it:edges){
-            int w = it.first;
-            int u = it.second.first;
-            int v = it.second.second;
+  // Sort according to the weight and weight is in 2nd index of edges array so
+  return a[2] < b[2];
+}
 
-            if(st.findParent(u)!= st.findParent(v)){ mxCnt+= w;
-            st.SizeByUnion(u,v);
+// Initialise the parent and rank vector
+void makeSet(vector<int> &parent,vector<int> &rank,int n)
+{
+  for(int i = 0;i<n;i++)
+  {
+    parent[i] = i;
+    rank[i] = 0;
+  }
+}
 
-            }
-        }
-        return mxCnt;
+// Find parent of nodes
+int findParents(int node,vector<int> &parent)
+{
+  // We start from the bottom node and go to the top
+  // if node is parent of itself means we have reached the top parent
+  if(parent[node] == node)
+  {
+    return node;
+  }
+
+  // parent[node] = findParents(parent[node],parent) is for path compression logic
+  return parent[node] = findParents(parent[node],parent);
+}
+
+// Find union of 2 nodes
+void findUnion(int u,int v,vector<int> &parent,vector<int> &rank)
+{
+  // Find the parent for both node u and v
+  u = findParents(u,parent);
+  v = findParents(v,parent);
+
+  // Now find rank for both the parents of u and v
+  if(rank[u] < rank[v])
+  {
+    // Make the bigger one, parent of smaller one
+    parent[u] = v;
+  }
+  else if(rank[v]<rank[u])
+  {
+    parent[v] = u;
+  }
+  else{
+    // Both rank are equal so make anyone parent of anyone and increment its rank
+    parent[v] = u;
+    rank[u]++;
+  }
 
 }
 
+int minimumSpanningTree(vector<vector<int>>& edges, int n)
+{
+  // We need to srt the edges array related to weight so we make a comparator
+  sort(edges.begin(),edges.end(),cmp);
+
+  // Make a parent vector
+  vector<int> parent(n);
+  // Make a rank vector
+  vector<int> rank(n);
+
+  // Initialise parent and rank vector
+  makeSet(parent,rank,n);
+
+  // Kruskal's Algo
+  // We need to return the total weight of minimum spanning tree so
+  int weight = 0;
+  // We run a loop and start processing for each element of sorted edges array
+  for(int i = 0;i<edges.size();i++)
+  {
+    // u means node 1
+    int u = findParents(edges[i][0],parent);
+    // v means node 2
+    int v = findParents(edges[i][1],parent);
+    // w means weight
+    int w = edges[i][2];
+
+    // if parents are not equal merge them and add the weight in final answer
+    if(u != v)
+    {
+      weight += w;
+      findUnion(u,v,parent,rank);
+    }
+    // else if parents are same do nothing
+  }
+
+  return weight;
+}
 int main()
 {
     return 0;
