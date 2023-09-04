e9f48e94df62c72c0cba69b2f330535274befca1
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2023-09-04 15:29:09 +0530

    Binary Tree Part 1(2)

--- Striver Sheet/Binary Tree/12.cpp
+++ Striver Sheet/Binary Tree/12.cpp
@@ -46,6 +46,68 @@ public:
     }
 };
 
+int getMaxWidth(TreeNode<int> *root)
+{
+    // Number of nodes between first and last node of any level is its width (including first and last node)
+    // if between any 2 nodes in a level, there are no right or left node for a particular node in previous level
+    // we have to assume a node there also, because we need max width so we need to consider a full binary tree if we have a first and a last node in a level
+    // Here we need to see for number of nodes between first and last node for each level
+    // So, level order traversal can be used here
+    // if we can do indexing in such a way that root has index=1, nevel level has index 2 and 3 (left to right) and so on
+    // This way we have a tree whose each level is indexed, so get width for a particular level, we do (last index - first index + 1)
+    // Indexing will be in such a way that
+    // if we use (0-based indexing) then root has index = 0 say i, left = 2*i+1 and right = 2*i + 2
+    // if we use (1-based indexing) then root has index = 1 say i, left = 2*i and right = 2*i+1 (intuition of formula: Segament tree, as each node has 2 children)
+    // but if we take this way of indexing, In a skew tree, we might get overflow in worst case
+    // so we need to take value of i in such a way that each level index start from 1,2,3,4,5....
+    // So we need to modify our value of i in that way
+    // i = i - min, where min = minimal index value in that level
+    // left = 2*i+1 and right = 2*i+2
+    // for level order we need a queue of pair<node,index> initally <root,0> as we start with index = 0
+    if (root == NULL)
+        return 0;
+    int ans = 0;
+    queue<pair<TreeNode<int> *, int>> q;
+    q.push({root, 0});
+    while (!q.empty())
+    {
+        int size = q.size();
+        // min element in each level
+        int mmin = q.front().second;
+        // to store first and last index
+        int first, last;
+
+        // the below loop will run for each level once
+        // say we are first at root, once we have stored the width for level of root
+        // we pop it out and push root->left and root->right
+        // now we run below loop for size = 2 for above 2 newly pushed elements
+        // now we pop them out and store their left and right and so on
+        for (int i = 0; i < size; i++)
+        {
+            TreeNode<int> *node = q.front().first;
+            // we do - mmin to avoid that overflow situation
+            long index = q.front().second - mmin;
+            q.pop();
+
+            // for first node
+            if (i == 0)
+                first = index;
+            // for last node
+            if (i == size - 1)
+                last = index;
+
+            if (node->left)
+                q.push({node->left, index * 2 + 1});
+            if (node->right)
+                q.push({node->right, index * 2 + 2});
+        }
+
+        ans = max(ans, last - first + 1);
+    }
+
+    return ans;
+}
+
 int main()
 {
     return 0;
